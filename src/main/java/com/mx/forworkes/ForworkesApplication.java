128dd3926fb36d72cc0c5c06e87e8915988e5e96
Ignacio  Cisneros Juarez <ignacio.cisneros@next-technologies.com.mx>
2022-01-31 11:20:23 -0600

    [add] se agrega configuracion de proyecto inicial.

---
+++ src/main/java/com/mx/forworkes/ForworkesApplication.java
@@ -0,0 +1,13 @@
+package com.mx.forworkes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForworkesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForworkesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mx/forworkes/ForworkesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.forworkes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForworkesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
